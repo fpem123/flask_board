78395a1636cb2c830d910a3d8208542147b90d33
Lee Hoseop <ghtjq3909@naver.com>
2021-10-29 17:15:16 +0900

    사이드 버튼 추가

---
+++ static/scripts/page_util.js
@@ -0,0 +1,15 @@
+function page_scroll_top() {
+    const wrapper = document.getElementsByClassName("wrapper")[0];
+    wrapper.scrollIntoView(true);
+}
+
+function page_scroll_bottom() {
+    const wrapper = document.getElementsByClassName("wrapper")[0];
+    wrapper.scrollIntoView(false);
+}
+
+function page_scroll_up() {
+}
+
+function page_scroll_down() {
+}
