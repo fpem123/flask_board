3cee2aa47d9c3ea8db1949d2cbbf33eea9566cc5
Lee Hoseop <ghtjq3909@naver.com>
2021-10-29 18:04:39 +0900

    리모트 버튼 완성

--- static/scripts/page_util.js
+++ static/scripts/page_util.js
@@ -9,7 +9,27 @@ function page_scroll_bottom() {
 }
 
 function page_scroll_up() {
+    let contents = document.getElementsByClassName("frame-contents");
+    contents = [...contents].reverse();
+    const scroll = document.documentElement.scrollTop;
+    for (const content of contents) { 
+        const top = content.getBoundingClientRect().top;
+        if (Math.ceil(top + scroll) < Math.ceil(scroll)) {
+            content.scrollIntoView(true);
+            break;
+        }
+    }
 }
 
 function page_scroll_down() {
+    const contents = document.getElementsByClassName("frame-contents");
+    const scroll = document.documentElement.scrollTop;
+    const vp_hegiht = window.innerHeight;
+    for (const content of contents) { 
+        const bottom = content.getBoundingClientRect().bottom;
+        if (Math.ceil(bottom + scroll) > Math.ceil(vp_hegiht + scroll)) {
+            content.scrollIntoView(false);
+            break;
+        }
+    }
 }
